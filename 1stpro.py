b2a3db4c386d69d64adf88694bff84690b1af340
PHOENIX-998 <rajarshidutta18@gmail.com>
2025-07-19 00:37:46 +0530

    hackerrank challenge

--- 1stpro.py
+++ 1stpro.py
@@ -1,2 +1,8 @@
-a = int(input("Enter a number: "))
-print("You entered:", a)
+if __name__ == '__main__':
+    record = []
+    name = []
+    for _ in range(int(input())):
+        record.append([input(),float(input())])
+    record = set(record)
+    print(record)
+    print(name)
