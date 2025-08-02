5bf8a60d2153e56f8687c56be0b0d435ae4df735
unknown <rajarshidutta18@gmail.com>
2025-08-02 17:16:38 +0530

    oop practice 3 use of __str__ function

--- oop.py
+++ oop.py
@@ -2,5 +2,7 @@ class Myclass:
     def __init__(self,name,age):
         self.name = name
         self.age = age
+    def __str__(self):
+        return f"{self.name}({self.age})"
 p1 = Myclass("Rajarshi",21)
-print(p1.age)
+print(p1)
